108774d44703a933135fca607c9a4997c51c474a
methzen <diamohaminsa@gmail.com>
2021-12-22 21:39:08 +0100

    initial commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    env: {
+      "DEV_FRONTEND_API_URL": "http://localhost:9000",
+      "DEV_ADMIN_API_URL": "http://localhost:9001",
+      "PRODUCTION_ADMIN_API_URL": "https://www.admin-api.mouhamadoudia.fr",
+      "PRODUCTION_FRONTEND_API_URL": "https://www.api.mouhamadoudia.fr",
+      "PRODUCTION_FRONTEND_WEBSITE_URL": "https://www.mouhamadoudia.fr"
+    }
+  }
